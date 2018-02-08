eeefcecc0a5b2bb63db41a18fabd8e70bf7beafa
Higor Cervelin <higor.cervelin@gmail.com>
2018-02-08 19:11:24 -0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'twitter-media-scraper',
+  packages = ['twitter-media-scraper'],
+  version = '0.1',
+  description = "A lib for download the user's twitter media",
+  author = 'Higor Cervelin',
+  author_email = 'higor.cervelin@gmail.com',
+  url = 'https://github.com/HigorCervelin/stalker/tree/twitter-scraper',
+  download_url = 'https://github.com/HigorCervelin/stalker/archive/0.1.tar.gz',
+  keywords = ['scrapping', 'scrap', 'scraper', 'twitter', 'media', 'download'],
+  classifiers = [],
+)
